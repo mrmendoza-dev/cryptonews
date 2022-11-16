f01293406c99f8e07badeeab3ac74c9a6aef6ea6
Steven Mendoza <smendoza171@gmail.com>
2022-11-16 11:06:37 -0800

    added updated Nav

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { useState, useEffect } from 'react'
 import './css/App.css'
-import "./css/Nav.css";
 import "./css/News.css";
-
 import {nanoid} from "nanoid"
 import logo from "./assets/logo.png";
-import DarkMode from "./components/DarkMode";
 import styled from "styled-components";
-
 import newsData from "./data/cryptonews_light.json";
-
+import Footer from "./components/Nav/Footer";
+import Header from "./components/Nav/Header";
 
 const Percent = styled.p<{ data: number }>`
   color: ${(props: any) =>
@@ -99,53 +96,7 @@ function App() {
 
   return (
     <div className="App">
-      <div className="Header">
-        <div className="main-header">
-          <a href="/">
-            <div className="header-title">
-              <img src={logo} />
-              <p>CryptoNews</p>
-            </div>
-          </a>
-          <DarkMode />
-        </div>
-
-        <div className="sub-header">
-          <p>
-            Coins: {Number(globalData.active_cryptocurrencies).toLocaleString()}
-          </p>
-          <p>Exchanges: {globalData.markets.toLocaleString()}</p>
-          <div className="market-change">
-            <p>
-              Market Cap: $
-              {globalData.total_market_cap.usd.toLocaleString(undefined, {
-                maximumFractionDigits: 0,
-              })}
-            </p>
-            <Percent data={globalData.market_cap_change_percentage_24h_usd}>
-              {globalData.market_cap_change_percentage_24h_usd.toFixed(2)}%
-              {globalData.market_cap_change_percentage_24h_usd > 0 ? (
-                <i className="fa-solid fa-caret-up"></i>
-              ) : (
-                <i className="fa-solid fa-caret-down"></i>
-              )}
-            </Percent>
-          </div>
-
-          <p>
-            24h Vol: $
-            {Number(globalData.total_volume.usd).toLocaleString(undefined, {
-              maximumFractionDigits: 0,
-            })}
-          </p>
-          <div className="crypto-dominance">
-            <p>Dominance:</p>
-            <p>BTC {globalData.market_cap_percentage.btc.toFixed(1)}%</p>
-            <p>ETH {globalData.market_cap_percentage.eth.toFixed(1)}%</p>
-          </div>
-        </div>
-        {/* <p>Gas: {globalData.active_cryptocurrencies}</p> */}
-      </div>
+      <Header globalData={globalData} title={"CryptoNews"} />
 
       <div className="News">
         <div className="news-video">
@@ -160,12 +111,10 @@ function App() {
               );
             })}
 
-              {/* <iframe
+            {/* <iframe
                 className="video-frame"
                 src={currentVideo.news_url.replace("watch?v=", "embed/")}
               ></iframe> */}
-
-
           </div>
         </div>
 
@@ -197,6 +146,7 @@ function App() {
           })}
         </div>
       </div>
+      <Footer title={"CryptoNews"} />
     </div>
   );
 }
