93f0b0a3260e1192090883d86810b1958f381cf4
Steven Mendoza <smendoza171@gmail.com>
2022-11-26 16:02:25 -0800

    fixed json data issue

--- src/App.tsx
+++ src/App.tsx
@@ -133,33 +133,46 @@ function App() {
               ></iframe> */}
           </div>
         </div>
-
         <div className="news-latest">
-          {articles.map((article) => {
-            return (
-              <div key={nanoid()} className="news-card">
-                <a
-                  className="news-img-box"
-                  href={article.news_url}
-                  target="_blank"
-                  rel="noopener"
-                >
-                  <img className="news-img" src={article.image_url} />
-                </a>
-
-                <div className="news-info">
-                  <a href={article.news_url} target="_blank" rel="noopener">
-                    <p className="news-title">{article.title}</p>
+          <p className="news-heading">Latest News</p>
+          <div className="news-list">
+            {articles.map((article) => {
+              return (
+                <div key={nanoid()} className="news-card">
+                  <a
+                    className="news-img-box"
+                    href={article.news_url}
+                    target="_blank"
+                    rel="noopener"
+                  >
+                    <img className="news-img" src={article.image_url} />
                   </a>
-                  <div className="news-subheader">
-                    <p className="news-source">{article.source_name}</p>
-                    <p className="news-date">{article.date}</p>
+
+                  <div className="news-info">
+                    <a href={article.news_url} target="_blank" rel="noopener">
+                      <p className="news-title">{article.title}</p>
+                    </a>
+                    <div className="news-subheader">
+                      <p className="news-source">{article.source_name}</p>
+                      <p className="news-date">{article.date}</p>
+                    </div>
+                    <p className="news-text">{article.text}</p>
+
+                    <div className="news-tickers">
+                      {article.tickers.map((ticker: any) => {
+                        return <p>{ticker}</p>;
+                      })}
+                    </div>
                   </div>
-                  <p className="news-text">{article.text}</p>
+                  <div className="news-bookmark">
+                    {/* <i className="fa-solid fa-bookmark"></i> */}
+
+                    <i className="fa-regular fa-bookmark"></i>
+                  </div>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
       </div>
       <Footer title={"CryptoNews"} />
